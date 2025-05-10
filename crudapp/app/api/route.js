0f322de2da967ee6b7a1a367f12ab25df0aae6a0
Richard Chekwas <richardchekwas@gmail.com>
2025-05-10 21:20:49 +0100

    update

--- crudapp/app/api/route.js
+++ crudapp/app/api/route.js
@@ -1,8 +1,15 @@
 import dbClient from '../../db';
+import { NextResponse } from 'next/server';
 
-const uu = async () => { 
-    const gg = await dbClient.client.db().collection('users').findOne({"email": "richardchekwas@gmail.com"});
-    console.log(gg);
+
+export async function GET(request) {
+    try {
+        const gg = await dbClient.client.db().collection('users').findOne({"email": "richardchekwas@gmail.com"});
+        console.log(gg);
+        return  NextResponse.json('{successssss}', {status: 201});
+    } catch {
+        console.log('err');
+    }
 };
 uu();
 
