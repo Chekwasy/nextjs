fc594798aa89a5562d981ef79aff4cde8b59ec32
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-12 15:15:11 +0100

    Update page.tsx

--- crudapp/app/auth/login/page.tsx
+++ crudapp/app/auth/login/page.tsx
@@ -100,6 +100,7 @@ function page() {
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
+              onClick={handleSubmit}
             >
               Login
             </button>
