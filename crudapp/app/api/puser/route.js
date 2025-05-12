5ae23b5012235dc73739b2210ef1357172946f7d
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-12 13:21:47 +0100

    Update route.js

--- crudapp/app/api/puser/route.js
+++ crudapp/app/api/puser/route.js
@@ -11,30 +11,32 @@ const makeID = () => {
 
 export async function POST(request) {
     try {
-        const { emailpwd, firstname, lastname } = await request.json();
-    	const encoded_usr_str = (emailpwd.split(" "))[1];
-	    let decoded_usr_str = '';
-		decoded_usr_str = Buffer.from(encoded_usr_str, 'base64').toString('utf-8');
+	const dd = await request.json();
+        const { emailpwd, firstname, lastname } = dd;
+	const encoded_usr_str = (emailpwd.split(" "))[1];
+	let decoded_usr_str = '';
+	decoded_usr_str = Buffer.from(encoded_usr_str, 'base64').toString('utf-8');
     	const usr_details = decoded_usr_str.split(':');
-	    const password = crypto.createHash('sha256').update(usr_details[1]).digest('hex');
-	    const userID = makeID();
-	    const email = usr_details[0];
-	    if (!email) {
-		    console.log(1, emailpwd, 1, firstname, 1, lastname, await request.json());
-		    return NextResponse.json('error', {status: 401});
-	    }
-	    if (!password) {
-		    console.log(2);
-		    return NextResponse.json('error', {status: 401});
-	    }
-	    const user = await (await dbClient.client.db().collection('users'))
-	    .findOne({ "email": email });
-	    if (user) {
-		    return NextResponse.json('user exists', {status: 404});
-	    }
-	    const result = await (await dbClient.client.db().collection('users'))
-	    .insertOne({userID: userID, email: email, password: password, firstname: firstname, lastname: lastname });
-	    if (result) {
+	const password = crypto.createHash('sha256').update(usr_details[1]).digest('hex');
+	const userID = makeID();
+	const email = usr_details[0];
+	if (!email) {
+		console.log(1, emailpwd, 1, firstname, 1, lastname, dd);
+		return NextResponse.json('error', {status: 401});
+	    
+	}
+	if (!password) {
+		console.log(2);
+		return NextResponse.json('error', {status: 401});
+	}
+	const user = await (await dbClient.client.db().collection('users'))
+	.findOne({ "email": email });
+	if (user) {
+		return NextResponse.json('user exists', {status: 404});
+	}
+	const result = await (await dbClient.client.db().collection('users'))
+	.insertOne({userID: userID, email: email, password: password, firstname: firstname, lastname: lastname });
+	if (result) {
             return NextResponse.json({'success': email}, {status: 201});
         }
     } catch {
