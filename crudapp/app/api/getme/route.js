dcf4d22af9f9eae415f21c0d02366a049076a852
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-13 16:51:55 +0100

    Update route.js

--- crudapp/app/api/getme/route.js
+++ crudapp/app/api/getme/route.js
@@ -14,8 +14,8 @@ export async function GET(request) {
 		    return  NextResponse.json('error', {status: 401});
 	    }
 	console.log(441, usr_id);
-	    const user = await (await dbClient.client.db().collection('users'))
-    	.findOne({ "_id": ObjectID(usr_id) });
+	    const user = await dbClient.client.db().collection('users')
+    	.findOne({ "userID": usr_id });
 	console.log(77771);
 	    if (!user) { return  NextResponse.json('error', {status: 401});}
         return  NextResponse.json({email: user.email}, {status: 201});
