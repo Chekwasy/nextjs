6e73ff3ad4bf0261785ec66b9643532df85f9efe
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-13 13:13:16 +0100

    Update route.js

--- crudapp/app/api/deleteworker/route.js
+++ crudapp/app/api/deleteworker/route.js
@@ -9,18 +9,16 @@ export async function DELETE(request) {
         const firstname = dd.headers.get('firstname');
         const lastname = dd.headers.get('lastname');
         const email = dd.headers.get('email');
-        if (!tok) { console.log(1); return NextResponse.json('error', {status: 400});}
+        if (!tok) { return NextResponse.json('error', {status: 400});}
         const usr_id = await redisClient.get(`auth_${tok}`);
         if (!usr_id) {
-            console.log(2);
-            return  NextResponse.json('error', {status: 400});
+            return  NextResponse.json('error', {status: 401});
         }
         const user = await (await dbClient.client.db().collection('workers'))
         .deleteOne({ email: email, firstname: firstname, lastname: lastname });
-        if (!user) { console.log(3); return NextResponse.json('error', {status: 400});}
+        if (!user) { return NextResponse.json('error', {status: 400});}
         return  NextResponse.json('success', {status: 201});
     } catch {
-        console.log(4);
         return  NextResponse.json('error', {status: 400});
     }
 };
