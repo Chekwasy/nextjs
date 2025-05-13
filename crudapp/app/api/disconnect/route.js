de067072252eda04ff89ac8124293da9522ac1e4
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-13 13:14:04 +0100

    Update route.js

--- crudapp/app/api/disconnect/route.js
+++ crudapp/app/api/disconnect/route.js
@@ -8,7 +8,7 @@ export async function GET(request) {
 		if (!tok) { return  NextResponse.json('error', {status: 400});}
 		const usr_id = await redisClient.get(`auth_${tok}`);
 		if (!usr_id) {
-			return  NextResponse.json('error', {status: 400});
+			return  NextResponse.json('error', {status: 401});
 		}
 		await redisClient.del(`auth_${tok}`);
         return  NextResponse.json('success', {status: 201});
