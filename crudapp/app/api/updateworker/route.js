d71d1213be5dbbb55e014a925fbe25890a40b375
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-13 10:47:52 +0100

    Update route.js

--- crudapp/app/api/updateworker/route.js
+++ crudapp/app/api/updateworker/route.js
@@ -6,12 +6,12 @@ import { ObjectID } from 'mongodb';
 export async function PUT(request) {
     const dd = await request.json();
     try {
-        console.log(dd);
-        const { tok, age, department, address, mobile,} = dd;
+        const { tok, age, department, address, mobile,} = await request.json();
         const lastupdate = new Date();
-        if (!tok) { console.log(1, dd ); return  NextResponse.json('error', {status: 400});}
+        if (!tok) { return  NextResponse.json('error', {status: 400});}
         const usr_id = await redisClient.get(`auth_${tok}`);
         if (!usr_id) {
+            console.log(1);
             return NextResponse.json('error', {status: 400});
         }
         const user = await (await dbClient.client.db().collection('workers'))
