db8128aa4809cf68ab757016ed0302625243f309
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-13 11:12:51 +0100

    Update route.js

--- crudapp/app/api/updateworker/route.js
+++ crudapp/app/api/updateworker/route.js
@@ -9,16 +9,17 @@ export async function PUT(request) {
         dd = await request.json();
         const { tok, age, department, address, mobile } = dd;
         const lastupdate = new Date();
-        console.log("hdhdhg");
         if (!tok) { return  NextResponse.json('error', {status: 400});}
         const usr_id = await redisClient.get(`auth_${tok}`);
         if (!usr_id) {
             return NextResponse.json('error', {status: 400});
         }
+        console.log("hdhdhg");
         const user = await (await dbClient.client.db().collection('workers'))
         .updateOne({ "_id": ObjectID(usr_id) }, 
-        { $set: { age: age, department: department, address: address, mobile: mobile, lastupdate: lastupdate} });       
-        if (!user) { return  NextResponse.json('error', {status: 400});}
+        { $set: { age: age, department: department, address: address, mobile: mobile, lastupdate: lastupdate} });
+        console.log("aaaaaaaaaaa");
+        if (!user) { return NextResponse.json('error', {status: 400});}
         return  NextResponse.json('success', {status: 201});
     } catch {
         console.log(4, dd);
