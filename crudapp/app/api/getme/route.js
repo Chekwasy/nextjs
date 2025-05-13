f933acfa7724de35b708110021177d4b1eda84ec
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-13 13:21:19 +0100

    Update route.js

--- crudapp/app/api/getme/route.js
+++ crudapp/app/api/getme/route.js
@@ -1,11 +1,11 @@
-import dbClient from '../../db';
+import dbClient from '../../../db';
 import { NextResponse } from 'next/server';
 import { ObjectID } from 'mongodb';
-import redisClient from '../../redis';
+import redisClient from '../../../redis';
 
 export async function GET(request) {
     try {
-        const { tok } = request.json();
+        const tok = await request.headers.get("tok");
 	    if (!tok) { return  NextResponse.json('error', {status: 400});}
     	const usr_id = await redisClient.get(`auth_${tok}`);
 	    if (!usr_id) {
@@ -13,7 +13,7 @@ export async function GET(request) {
 	    }
 	    const user = await (await dbClient.client.db().collection('users'))
     	.findOne({ "_id": ObjectID(usr_id) });
-	    if (!user) { return  NextResponse.json('error', {status: 400});}
+	    if (!user) { return  NextResponse.json('error', {status: 401});}
         return  NextResponse.json({email: user.email}, {status: 201});
     } catch {
         return  NextResponse.json('error', {status: 400});
