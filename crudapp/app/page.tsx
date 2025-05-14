045dd995f67fb6691e34e99064dce5be7b64d7db
Richard Chekwas <richardchekwas@gmail.com>
2025-05-14 07:36:56 +0100

    update

--- crudapp/app/page.tsx
+++ crudapp/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
@@ -11,19 +12,19 @@ export default function Home() {
   const [userEmail, setUserEmail] = React.useState('');
   const checkLogged = () => {
     axios.get('/api/getme', {
-          headers: {
+      headers: {
         tok: Cookies.get('tok'),
-        }})
-      .then(async (response: Response) => {
-          setUserEmail(response.data.email);
-          setLogged(true);
-      })
-      .catch(error => {
-      });
+    }})
+    .then(async (response: Response) => {
+        setUserEmail(response.data.email);
+        setLogged(true);
+    })
+    .catch(error => {
+    });
   };
   React.useEffect(() => {
-  checkLogged();
-}, []);
+    checkLogged();
+  }, []);
   return (
     <div className="bg-cover bg-center h-screen w-screen"
       style={{
@@ -31,90 +32,117 @@ export default function Home() {
       }}
       >
       <nav className='bg-gray-800 py-4 fixed top-0 left-0 w-full z-10'>
-  <div className='container mx-auto px-4 flex justify-between'>
-    <Link href={'/'} className='md:flex-shrink-0 flex items-top'>
-      <div className='text-lg font-bold text-white'>CrudApp</div>
-    </Link>
-    <ul className='md:flex hidden items-center space-x-4'>
-      <li>
-        <Link href={'/create'}>
-          <div className='text-gray-300 hover:text-white flex items-center'>Create</div>
-        </Link>
-      </li>
-      <li>
-        <Link href={'/read'}>
-          <div className='text-gray-300 hover:text-white flex items-center'>Read</div>
-        </Link>
-      </li>
-      <li>
-        <Link href={'/update'}>
-          <div className='text-gray-300 hover:text-white flex items-center'>Update</div>
-        </Link>
-      </li>
-      <li>
-        <Link href={'/delete'}>
-          <div className='text-gray-300 hover:text-white flex items-center'>Delete</div>
-        </Link>
-      </li>
-    </ul>
-    <div className='flex items-center space-x-4'>
-      <div className='text-gray-300'>{userEmail}</div>
-      <div onClick={() => checkLogged()} className='w-10 h-10 rounded-full bg-gray-300 overflow-hidden'>
-        <img src={'/images/landing-background.svg'} alt='Profile Picture' />
+        <div className='container mx-auto px-4 flex justify-between'>
+          <Link href={'/'} className='md:flex-shrink-0 flex items-top'>
+            <div className='text-lg font-bold text-white'>CrudApp</div>
+          </Link>
+          { logged && (<div className='flex items-center space-x-4'>
+            <div className='text-gray-300'>{userEmail}</div>
+            <div onClick={() => checkLogged()} className='w-10 h-10 rounded-full bg-gray-300 overflow-hidden'>
+              <Image src={'/images/landing-background.svg'} alt='Profile Picture' layout='fill' objectFit='cover' />
+            </div>
+          </div>)}
+          <ul className='md:flex hidden items-center space-x-4'>
+            <li>
+              <Link href={'/create'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Create</div>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/read'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Read</div>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/update'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Update</div>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/delete'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Delete</div>
+              </Link>
+            </li>
+          </ul>
+
+          {!menuOpen && (
+            <button
+              className='md:hidden text-gray-200 flex items-center justify-center w-8 h-8 hover:text-white'
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              Menu
+            </button>
+          )}
+          {menuOpen && (
+            <ul
+              className='md:hidden flex flex-col items-center justify-center bg-gray-400 text-gray-300 absolute top-full right-0 w-48 py-2 border border-gray-700 z-20'
+            >
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <button
+                  className='md:hidden flex items-center justify-center w-8 h-8 hover:text-white'
+                  onClick={() => setMenuOpen(!menuOpen)}
+                >
+                  X
+                </button>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/create'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Create
+                  </div>
+                </Link>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/read'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Read
+                  </div>
+                </Link>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/update'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Update
+                  </div>
+                </Link>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/delete'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Delete
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <div className='bg-white rounded-lg shadow-lg p-8 w-1/2'>
+          <h1 className='text-3xl font-bold text-gray-800 mb-4'>Add Worker Details</h1>
+          <p className='text-gray-700 text-lg mb-4'>
+            {`Welcome to our worker management system. To get started, please click on the "Add Worker" button below.`}
+          </p>
+          <p className='text-gray-700 text-lg mb-4'>
+            {`If you already have an account, please login to access our system. If not, please signup to create a new account.`}
+          </p>
+          <div className='mt-4'>
+            <p className='text-gray-700 text-sm'>
+              Already have an account?{' '}
+              <Link className='text-blue-500 hover:text-blue-700' href='/auth/login'>
+                Login
+              </Link>
+            </p>
+            <p className='text-gray-700 text-sm'>
+              Dont have an account?{' '}
+              <Link className='text-blue-500 hover:text-blue-700' href='/auth/signup'>
+                Signup
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-    {!menuOpen && (
-      <button
-        className='md:hidden text-gray-200 flex items-center justify-center w-8 h-8 hover:text-white'
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        Menu
-      </button>
-    )}
-    {menuOpen && (
-      <ul
-        className='md:hidden flex flex-col items-center justify-center bg-gray-400 text-gray-300 absolute top-full right-0 w-48 py-2 border border-gray-700 z-20'
-      >
-        <li className='px-4 py-2 hover:bg-gray-700'>
-          <button
-            className='md:hidden flex items-center justify-center w-8 h-8 hover:text-white'
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            X
-          </button>
-        </li>
-        <li className='px-4 py-2 hover:bg-gray-700'>
-          <Link href={'/create'}>
-            <div className='text-gray-100 hover:text-white flex items-center'>
-              Create
-            </div>
-          </Link>
-        </li>
-        <li className='px-4 py-2 hover:bg-gray-700'>
-          <Link href={'/read'}>
-            <div className='text-gray-100 hover:text-white flex items-center'>
-              Read
-            </div>
-          </Link>
-        </li>
-        <li className='px-4 py-2 hover:bg-gray-700'>
-          <Link href={'/update'}>
-            <div className='text-gray-100 hover:text-white flex items-center'>
-              Update
-            </div>
-          </Link>
-        </li>
-        <li className='px-4 py-2 hover:bg-gray-700'>
-          <Link href={'/delete'}>
-            <div className='text-gray-100 hover:text-white flex items-center'>
-              Delete
-            </div>
-          </Link>
-        </li>
-      </ul>
-    )}
-  </div>
-</nav>
+
     </div>
   );
 }
