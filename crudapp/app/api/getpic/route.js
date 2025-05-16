018c7c55878643c9f8fae1e4cc01251ae6289cfa
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-16 14:51:37 +0100

    Update route.js

--- crudapp/app/api/getpic/route.js
+++ crudapp/app/api/getpic/route.js
@@ -1,1 +1,115 @@
-gfdgtf
+import dbClient from '../../../db';
+import { NextResponse } from 'next/server';
+import redisClient from '../../../redis';
+import { tmpdir } from 'os';
+import { join as joinPath } from 'path';
+import {
+  mkdir, stat, existsSync, realpath, readFileSync,
+} from 'fs';
+import mime from 'mime-types';
+
+
+
+export async function GET(request) {
+  try {
+    const tok = request.cookies.get('tok') || request.headers.get('tok');
+    const size = '100';
+    const baseDir1 = joinPath(tmpdir(), '/crudapp/profilepicimages');
+    const mkDirAsync = promisify(mkdir);
+    const realpathAsync = promisify(realpath);
+    const defaultPath = joinPath(baseDir1, 'default');
+    if (!tok) {
+      await mkDirAsync(baseDir1, { recursive: true });
+      const absoluteFilePath = await realpathAsync(defaultPath);
+      const mimeType1 = mime.lookup(defaultPath);
+      return NextResponse.next({
+      status: 200,
+      headers: {
+        'Content-Type': contentType(mimeType1) || 'text/plain; charset=utf-8',
+      },
+      body: await readFileSync(absoluteFilePath),
+    });
+    }
+
+    const userID = await redisClient.get(`auth_${tok}`);
+
+    if (!userID) {
+      await mkDirAsync(baseDir1, { recursive: true });
+      const absoluteFilePath = await realpathAsync(defaultPath);
+      const mimeType1 = mime.lookup(defaultPath);
+      return NextResponse.next({
+      status: 200,
+      headers: {
+        'Content-Type': contentType(mimeType1) || 'text/plain; charset=utf-8',
+      },
+      body: await readFileSync(absoluteFilePath),
+    });
+    }
+
+    const file = await dbClient.client.db().collection('files').findOne({ userID: userID });
+
+    //Handle the default profile pic
+    if (!file) {
+      await mkDirAsync(baseDir1, { recursive: true });
+      const absoluteFilePath = await realpathAsync(defaultPath);
+      const mimeType1 = mime.lookup(defaultPath);
+      return NextResponse.next({
+      status: 200,
+      headers: {
+        'Content-Type': contentType(mimeType1) || 'text/plain; charset=utf-8',
+      },
+      body: await readFileSync(absoluteFilePath),
+    });
+    }
+
+    let filePath = file.localPath;
+
+    if (size) {
+      filePath = `${file.localPath}_${size}`;
+    }
+
+    if (existsSync(filePath)) {
+      const statAsync = promisify(stat);
+      const fileInfo = await statAsync(filePath);
+
+      if (!fileInfo.isFile()) {
+        await mkDirAsync(baseDir1, { recursive: true });
+        const absoluteFilePath = await realpathAsync(defaultPath);
+        const mimeType1 = mime.lookup(defaultPath);
+        return NextResponse.next({
+          status: 200,
+          headers: {
+            'Content-Type': contentType(mimeType1) || 'text/plain; charset=utf-8',
+          },
+          body: await readFileSync(absoluteFilePath),
+        });
+      }
+        } else {
+      await mkDirAsync(baseDir1, { recursive: true });
+      const absoluteFilePath = await realpathAsync(defaultPath);
+      const mimeType1 = mime.lookup(defaultPath);
+      return NextResponse.next({
+        status: 200,
+        headers: {
+          'Content-Type': contentType(mimeType1) || 'text/plain; charset=utf-8',
+        },
+        body: await readFileSync(absoluteFilePath),
+      });
+    }
+
+    const realpathAsync = promisify(realpath);
+    const absoluteFilePath = await realpathAsync(filePath);
+    const mimeType = mime.lookup(filePath);
+
+    return NextResponse.next({
+      status: 200,
+      headers: {
+        'Content-Type': contentType(mimeType) || 'text/plain; charset=utf-8',
+      },
+      body: await readFileSync(absoluteFilePath),
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error' }, { status: 400 });
+  }
+}
