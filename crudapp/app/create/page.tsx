52e8575e480c7f59746b205555dc516cffe1af00
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-16 18:45:38 +0100

    Update page.tsx

--- crudapp/app/create/page.tsx
+++ crudapp/app/create/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link';
-import React from 'react';
+import React from '../page';
 import { useState } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
