5f8631bef50f082c0b297bbd3691336863a0145c
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-17 12:19:30 +0100

    Update route.js

--- crudapp/app/api/picpush/route.js
+++ crudapp/app/api/picpush/route.js
@@ -6,13 +6,30 @@ import { tmpdir } from 'os';
 import { join as joinPath } from 'path';
 import { mkdir, writeFile } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
+import formidable from 'formidable';
 
 const mkDirAsync = promisify(mkdir);
 const writeFileAsync = promisify(writeFile);
 
 export async function POST(request) {
   try {
-    const { tok, image, name, type } = await request.json();
+    const form = new formidable.IncomingForm();
+    form.uploadDir = tmpdir();
+    form.keepExtensions = true;
+    form.multipart = true;
+
+    const parsedForm = await new Promise((resolve, reject) => {
+      form.parse(request, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ fields, files });
+        }
+      });
+    });
+
+    const { tok, name, type } = parsedForm.fields;
+    const image = parsedForm.files.image;
 
     if (!tok) {
       return NextResponse.json('error', { status: 400 });
@@ -33,6 +50,7 @@ export async function POST(request) {
     if (!user) {
       return NextResponse.json('error', { status: 401 });
     }
+
     const filer = await dbClient.client.db().collection('files').findOne({ userID: usr_id });
 
     if (filer) {
@@ -54,8 +72,8 @@ export async function POST(request) {
     await mkDirAsync(baseDir, { recursive: true });
 
     if (type === 'image') {
-      const localPath = joinPath(baseDir, uuidv4());
-      await writeFileAsync(localPath, Buffer.from(image, 'base64'));
+      const localPath = joinPath(baseDir, image.originalFilename);
+      await writeFileAsync(localPath, image.file);
       newFile.localPath = localPath;
     }
 
