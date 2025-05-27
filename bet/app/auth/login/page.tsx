af22fbee14d658033b010ac9fc70b6c7ea5506f0
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-05-27 03:05:17 +0100

    Update page.tsx

--- bet/app/auth/login/page.tsx
+++ bet/app/auth/login/page.tsx
@@ -93,37 +93,19 @@ const handleClose = () => {
         <div className="bg-gray-500 rounded-lg shadow-lg p-8 xs:w-full sm:w-1/2 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
           <h2 className="text-3xl font-bold text-blue-500 mb-4">Admin Login</h2>
           {isOpen && (
-        <div
-          className="popup-overlay fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center"
-          onClick={handleOverlayClick}
-        >
-          <div
-            className="popup-content bg-white rounded-lg shadow-md p-4 w-1/2"
-          >
-            <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-              onClick={handleClose}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-
-            <h2 className="text-lg font-bold mb-4">{setMsg}</h2>
-          </div>
-        </div>
-      )}
+            <div className="popup-overlay fixed top-0 left-0 w-full h-full bg-transparent flex items-center justify-center" onClick={handleOverlayClick}>
+              <div className="popup-content bg-white rounded-lg shadow-md p-8 w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4" >
+                <div className="flex justify-end">
+                  <button className="text-gray-500 hover:text-gray-700" onClick={handleClose} >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+                <h2 className="text-lg font-bold mb-4">{setMsg}</h2>
+              </div>
+            </div>
+          )}
           <form onSubmit={handleLSubmit}>
             <div className="mb-4">
               <span className="block sm:inline">{'Do not use these symbols \' " : + \\ `'}</span>
