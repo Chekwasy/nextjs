daa12aa1c35418f734cde097f1c87052aa9685ee
Richard Chekwas <richardchekwas@gmail.com>
2025-05-29 14:45:59 +0100

    update

--- bet/app/page.tsx
+++ bet/app/page.tsx
@@ -1,103 +1,126 @@
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect, ChangeEvent } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="bg-cover bg-center h-screen w-screen"
+      >
+      <nav className='bg-green-800 py-4 fixed top-0 left-0 w-full z-10'>
+        <div className='container mx-auto px-4 flex justify-between'>
+          <Link href={'/'} className='md:flex-shrink-0 flex items-top'>
+            <div className='text-lg font-bold text-white'>TryBet</div>
+          </Link>
+          <div className="flex items-center space-x-4">
+            <button
+              onClick={() => { const imgUpload = document.getElementById('image-upload'); if (imgUpload) {imgUpload.click()}}}
+              className="w-10 h-10 rounded-full bg-gray-300 overflow-hidden"
+            >
+              <Image
+                src="/api/getpic"
+                alt="Upload Image"
+                layout="fixed"
+                width={40}
+                height={40}
+                objectFit="cover"
+                className="rounded-full"
+                key={picUpdated}
+              />
+            </button>
+          </div>
+          <input
+            id="image-upload"
+            type="file"
+            onChange={handleImageUpload}
+            accept="image/*"
+            style={{ display: 'none' }}
+          />
+          { logged && (<div className='flex items-center space-x-4'>
+            <div className='text-gray-300'>{userEmail}</div>
+          </div>)}
+          <ul className='md:flex hidden items-center space-x-4'>
+            <li>
+              <Link href={'/create'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Create</div>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/read'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Read</div>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/update'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Update</div>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/delete'}>
+                <div className='text-gray-300 hover:text-white flex items-center'>Delete</div>
+              </Link>
+            </li>
+            {logged && (<li>
+              <div onClick={() => handleLogout()} className='text-gray-300 hover:text-red-700 cursor-pointer flex items-center'>Logout</div>
+            </li>)}
+          </ul>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {!menuOpen && (
+            <button
+              className='md:hidden text-gray-200 flex items-center justify-center w-8 h-8 hover:text-white'
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              Menu
+            </button>
+          )}
+          {menuOpen && (
+            <ul
+              className='md:hidden flex flex-col items-center justify-center bg-gray-400 text-gray-300 absolute top-full right-0 w-48 py-2 border border-gray-700 z-20'
+            >
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <button
+                  className='md:hidden flex items-center justify-center w-8 h-8 hover:text-white'
+                  onClick={() => setMenuOpen(!menuOpen)}
+                >
+                  X
+                </button>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/create'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Create
+                  </div>
+                </Link>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/read'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Read
+                  </div>
+                </Link>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/update'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Update
+                  </div>
+                </Link>
+              </li>
+              <li className='px-4 py-2 hover:bg-gray-700'>
+                <Link href={'/delete'}>
+                  <div className='text-gray-100 hover:text-white flex items-center'>
+                    Delete
+                  </div>
+                </Link>
+              </li>
+              {logged && (<li>
+                <div onClick={() => handleLogout()} className='text-gray-100 hover:text-red-700 cursor-pointer flex items-center'>Logout</div>
+              </li>)}
+            </ul>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </nav>
     </div>
   );
 }
