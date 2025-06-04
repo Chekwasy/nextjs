98b1bb32a03b376402e8fa2c50d04d11a2c7152d
Richard Chekwas <richardchekwas@gmail.com>
2025-06-04 19:41:03 +0100

    update

--- bet/app/page.tsx
+++ bet/app/page.tsx
@@ -139,11 +139,11 @@ export default function Home() {
             <li>
               <Link href={'/'}>
                 <div className='bg-green-900 hover:bg-green-200 rounded text-gray-700 hover:text-white flex items-center relative'>
-                  <Image src="/icons/home.png" alt="Home" width={35} height={35} />
-                  <div className="absolute group-hover:visible transition-opacity duration-200 -mt-6 ml-6 z-10">
-                    <span className="bg-gray-800 text-gray-100 text-sm px-2 py-1 rounded">
-                      Home
-                    </span>
+                  <div className="group">
+                    <Image src="/icons/home.png" alt="Home" width={35} height={35} />
+                    <div className="absolute invisible group-hover:visible transition-opacity duration-200 -mt-6 ml-6">
+                      <span className="bg-gray-800 text-gray-100 text-sm px-2 py-1 rounded"> Home </span>
+                    </div>
                   </div>
                 </div>
               </Link>
