35c39c0de9a4fef1998d6acef0734c476abe35c8
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-07 11:38:01 +0100

    Create redis.js

---
+++ bet/redis.js
@@ -0,0 +1,49 @@
+import { createClient } from 'redis';
+const client = createClient({
+  socket: {
+    port: 6379,
+    host: 'localhost',
+  },
+});
+
+class RedisClient {
+        constructor() {
+                this.client = this.connects();
+        }
+
+        isAlive() {
+                return this.clientConnected;
+        }
+
+        async connects() {
+                try {
+                        await client.connect();
+                        await client.ping();
+                        this.clientConnected = true;
+                        return client;
+                } catch (err) {
+			console.log(err.message);
+                        this.clientConnected =false;
+                        return cli;
+                }
+        }
+
+        async get(key) {
+		return await client.get(key);
+	}
+
+        async set(key, value, duration) {
+		await client.set(key, value, {
+  			EX: duration,
+  			NX: true
+		});
+	}
+
+        async del(key) {
+		await client.del(key);
+	}
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
