164cfea573748258841981f7bd0f9c6fb4c20768
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-08 19:40:03 +0100

    Update page.tsx

--- bet/app/page.tsx
+++ bet/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import Nav from '@/app/components/nav';
+import Main from '@/app/components/main';
 
 export default function Home() {
   return (
     <div className="bg-cover bg-center h-screen w-screen"
       >
       <Nav/>
+      <Main/>
     </div>
   );
 }
