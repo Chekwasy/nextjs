506e8b6989b60f4dc1a23ab8b4c823c116fedc89
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-11 05:15:15 +0100

    Update main.tsx

--- bet/app/components/main.tsx
+++ bet/app/components/main.tsx
@@ -93,8 +93,6 @@ const load = async () => {
         <button onClick={handleNext}><Image src="/icons/front.svg" alt="Next" width={30} height={30} /></button>
       </div>
     </div>
-	      <div className="hidden lg:block">
-	      </div>
         <button aria-label="Open sidebar" onClick={() => setSidebarOpen(!sidebarOpen)}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
