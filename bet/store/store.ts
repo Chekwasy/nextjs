922e541511458db12d52953cf7ec574805745527
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-12 13:42:59 +0100

    Update store.ts

--- bet/store/store.ts
+++ bet/store/store.ts
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-export const makeStore = () => {
-  return configureStore({
+export const store = configureStore({
     reducer: {}
   })
 }
 
-// Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
+export type RootState = ReturnType<store['getState']>
 export type AppDispatch = AppStore['dispatch']
