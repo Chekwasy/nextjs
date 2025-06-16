43b345654def6bc212c6ba6f96e501803cf2fdb1
Richard Chekwas <richardchekwas@gmail.com>
2025-06-16 18:36:57 +0100

    update

--- bet/app/components/main.tsx
+++ bet/app/components/main.tsx
@@ -438,7 +438,7 @@ const pyd: {
         </div>
       </div>
       {/* Sidebar */}
-      <div className={`absolute top-0 right-0 h-screen w-96 bg-white shadow-lg border-4 rounded-lg border-green-300 ${sidebarOpen ? 'block' : 'hidden'}`} id="sidebar" style={{ top: '65px' }}>
+      <div className={`absolute top-0 right-0 h-screen w-96 overflow-y-auto bg-white shadow-lg border-4 rounded-lg border-green-300 ${sidebarOpen ? 'block' : 'hidden'}`} id="sidebar" style={{ top: '65px' }}>
         <div className="absolute top-0 left-0 w-full bg-green-500 text-white p-4 rounded-t-lg flex justify-between items-center">
           <h2 className="font-bold text-lg">Sidebar Heading</h2>
           <button aria-label="Close sidebar" onClick={() => setSidebarOpen(false)}>
@@ -456,7 +456,7 @@ const pyd: {
               </div>
               <h3 className="font-bold text-lg">{item.hometeam} vs {item.awayteam}</h3>
               <div className='flex justify-between'>
-                <p className='w-1/3'>{item.mTime}</p>
+                <p className='w-1/3'>{`${item.mTime.substring(8, 10)}:${item.mTime.substring(10, 12)}`}</p>
                 <p className='w-1/3'>{item.selection}</p>
                 <h4 className='w-1/3 font-bold'>{item.odd}</h4>
               </div>
