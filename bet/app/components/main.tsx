581a7589fc839ba97189b0f32ea4792129916d8f
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-18 13:04:42 +0100

    Update main.tsx

--- bet/app/components/main.tsx
+++ bet/app/components/main.tsx
@@ -275,7 +275,7 @@ export default function Main() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-          <div className="rounded-full mt--10 bg-red-700 text-white>{storeItems.mainSlice.played.length === 0 ? '' : storeItems.mainSlice.played.length}</div>
+          <div className="rounded-full mt--10 bg-red-700 text-white ">{storeItems.mainSlice.played.length === 0 ? '' : storeItems.mainSlice.played.length}</div>
         </button>
       </div>
       <div className="p-4 mt-12">
