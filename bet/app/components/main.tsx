afb2a23c0d2ba83ce2f653b3a9f748ab8215cb36
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-19 13:47:44 +0100

    Update main.tsx

--- bet/app/components/main.tsx
+++ bet/app/components/main.tsx
@@ -42,7 +42,6 @@ export default function Main() {
   const storeItems: StoreState = useSelector((state) => state) as StoreState;
   //control to sidebar
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [bookedOpen, setBookedOpen] = useState(false);
   const [msg, setMsg] = useState('This for popup message!');
   //control message open or close
   const [isOpen, setIsOpen] = useState(false);
@@ -108,9 +107,6 @@ export default function Main() {
   //handle close message popup
   const handleClose = () => {
     setIsOpen(false);
-  };
-  const handleCloseBooking = () => {
-    setBookedOpen(false);
   };
   //handles date selected from list
   const handleDateSelect = (date: string, indent: number) => {
@@ -361,20 +357,6 @@ export default function Main() {
               </button>
             </div>
             <h2 className="text-lg font-bold mb-4">{msg}</h2>
-          </div>
-        </div>
-      )}
-      {bookedOpen && (
-        <div className=" fixed top-0 left-0 w-full h-full bg-transparent flex items-center justify-center">
-          <div className="popup-content bg-gray-200 rounded-lg shadow-md p-8 w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4" >
-            <div className="flex justify-end">
-              <button className="text-gray-500 hover:text-gray-700" onClick={handleCloseBooking} >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-            <h2 className="text-lg font-bold mb-4">Booked</h2>
           </div>
         </div>
       )}
