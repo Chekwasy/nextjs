9074153ebd4d017ca4890ac95323c7230a72916b
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-19 13:43:01 +0100

    Update route.js

--- bet/app/api/connect/route.js
+++ bet/app/api/connect/route.js
@@ -1,21 +1,30 @@
 import dbClient from '../../../db';
 import { NextResponse } from 'next/server';
 import redisClient from '../../../redis';
+import { v4 } from 'uuid';
+import crypto from 'crypto';
 
-export async function GET(request) {
-	const dd = await request;
-	try {
-        const tok = dd.headers.get("tok");
-	    if (!tok) { return  NextResponse.json('error', {status: 400});}
-    	const usr_id = await redisClient.get(`auth_${tok}`);
-	    if (!usr_id) {
-		    return  NextResponse.json('error', {status: 401});
-	    }
-	    const user = await dbClient.client.db().collection('users')
-    	.findOne({ "userID": usr_id });
-	    if (!user) { return  NextResponse.json('error', {status: 401});}
-        return  NextResponse.json({email: user.email, message: "Login Successful"}, {status: 201});
+
+
+
+export async function POST(request) {
+    try {
+	const { auth_header } = await request.json();
+	if (!auth_header) { return NextResponse.json('error', {status: 400})}
+	const encoded_usr_str = (auth_header.split(" "))[1];
+	let decoded_usr_str = '';
+	decoded_usr_str = Buffer.from(encoded_usr_str, 'base64').toString('utf-8');
+	const usr_details = decoded_usr_str.split(':');
+	const pwd = crypto.createHash('sha256').update(usr_details[1]).digest('hex');
+	const email = usr_details[0];
+	const user = await (await dbClient.client.db().collection('users'))
+	.findOne({ "email": email });
+	if (user && (user.password === pwd)) {
+		const auth_token = v4();
+		redisClient.set(`auth_${auth_token}`, user.userID, 7 * 24 * 60 * 60);
+		return NextResponse.json({token: auth_token, message: "Login Successful"}, {status: 201});
+	}
     } catch {
-        return  NextResponse.json('error', {status: 400});
+	return NextResponse.json('error', {status: 400});
     }
 };
