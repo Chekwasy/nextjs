89dd7890094bc8b8528523502ecd87be3251995f
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-23 13:47:11 +0100

    Update route.js

---
+++ bet/app/api/disconnect/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import redisClient from '../../../redis';
+
+
+export async function GET(request) {
+    const dd = await request;
+    try {
+        const tok = dd.headers.get('tok');
+		if (!tok) { return  NextResponse.json('error', {status: 400});}
+		const usr_id = await redisClient.get(`auth_${tok}`);
+		if (!usr_id) {
+			return  NextResponse.json('error', {status: 401});
+		}
+		await redisClient.del(`auth_${tok}`);
+        return  NextResponse.json({message: 'Logged Out Successful'}, {status: 201});
+    } catch {
+        return  NextResponse.json('error', {status: 400});
+    }
+};
