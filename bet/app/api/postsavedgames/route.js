0cb748ff43226fb58db03da84cf963b6b6bfd301
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-26 15:38:45 +0100

    Update route.js

--- bet/app/api/postsavedgames/route.js
+++ bet/app/api/postsavedgames/route.js
@@ -2,19 +2,22 @@ import dbClient from '../../../db';
 import { NextResponse } from 'next/server';
 import redisClient from '../../../redis';
 
-export async function GET(request) {
-	const dd = await request;
-	try {
-        const tok = dd.headers.get("tok");
-	    if (!tok) { return  NextResponse.json('error', {status: 400});}
-    	const usr_id = await redisClient.get(`auth_${tok}`);
-	    if (!usr_id) {
-		    return  NextResponse.json('error', {status: 401});
-	    }
-	    const user = await dbClient.client.db().collection('users')
-    	.findOne({ "userID": usr_id });
-	    if (!user) { return  NextResponse.json('error', {status: 401});}
-        return  NextResponse.json({email: user.email}, {status: 201});
+export async function POST(request) {
+    let dd = await request;
+    try {
+        const tok = dd.headers.get('tok');
+	const savedgames = dd.headers.get('savedgames');
+	const savedbuttons = dd.headers.get('savedbuttons);
+        if (!tok || !savedgames || !savedbuttons) { return NextResponse.json('error', {status: 400});}
+        const usr_id = await redisClient.get(`auth_${tok}`);
+        if (!usr_id) {
+            return  NextResponse.json('error', {status: 401});
+        }
+	    here
+        const user = await (await dbClient.client.db().collection('users'))
+        .deleteOne({ email: email, firstname: firstname, lastname: lastname });
+        if (!user) { return NextResponse.json('error', {status: 400});}
+        return  NextResponse.json('success', {status: 201});
     } catch {
         return  NextResponse.json('error', {status: 400});
     }
