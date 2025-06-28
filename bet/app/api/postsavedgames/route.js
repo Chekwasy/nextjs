88615e10f31f6af42109ecc50f6c674479f8c367
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-28 20:50:56 +0100

    Update route.js

--- bet/app/api/postsavedgames/route.js
+++ bet/app/api/postsavedgames/route.js
@@ -13,30 +13,23 @@ try {
         if (!usr_id) {
             return  NextResponse.json('error', {status: 401});
         }
-	console.log("thd");
 	const usr_game = await dbClient.client.db().collection('savedgames')
     	.findOne({ "userID": usr_id });
 	if (!usr_game) {
-		console.log("gg");
 		const result = await (await dbClient.client.db().collection('savedgames'))
 		.insertOne({userID: usr_id, savedgames: savedgames, savedbuttons: savedbuttons,});
 		if (result) {
-			console.log("kk");
 			return NextResponse.json({message: "Save Successful"}, {status: 201});
 		}
-		console.log("pp");
 		return NextResponse.json('error', {status: 401});
 	} else {
-		console.log("uu");
 		const sa = await (await dbClient.client.db().collection('savedgames'))
 		.updateOne({ userID: usr_id }, 
 		{ $set: { savedgames: savedgames, savedbuttons: savedbuttons,} });
 		if (!sa) { return NextResponse.json('error', {status: 400});}
-		console.log("saved");
 		return  NextResponse.json({message: 'Save Successful'}, {status: 201});
 	}
     } catch {
-	console.log("yy");
         return  NextResponse.json('error', {status: 400});
     }
 };
