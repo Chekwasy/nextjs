692a61b8ef4b037670dbf5f1e8db2f5a2b86a296
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-06-29 19:41:45 +0100

    Update route.js

--- bet/app/api/getdate/route.js
+++ bet/app/api/getdate/route.js
@@ -3,10 +3,12 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
     try {
       const today = new Date();
-      const day = today.getDate();
+      const day = today.getDate().toString().padStart(2, '0');
       const hour = today.getHours();
       const minute = today.getMinutes();
-      return  NextResponse.json({day: day, hour: hour, minute: minute }, {status: 201});
+        const month = (today.getMonth() + 1).toString().padStart(2, '0');
+        const year = today.getFullYear().toString();
+      return  NextResponse.json({year: year, month: month, day: day, hour: hour, minute: minute }, {status: 201});
     } catch {
         return NextResponse.json('error', {status: 400});
     }
