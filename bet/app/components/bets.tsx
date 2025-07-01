f260f78718552649ea1f656525d8993d8644b392
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-01 20:06:57 +0100

    Update bets.tsx

--- bet/app/components/bets.tsx
+++ bet/app/components/bets.tsx
@@ -40,21 +40,28 @@ export default function Bets() {
   }, []);
   return (
     <div className="flex justify-center items-center mt-16">
-      <div className="md:w-4/5 w-11/12">
-        <div className="bg-gray-400 rounded-lg p-4 flex gap-4">
-          <div className=`${betTab === 'open' ? 'bg-green-200' : 'bg-white'} text-gray-700 text-center rounded-lg border-4 border-green-500 hover:border-green-200 p-4 w-1/2` onClick={() => handleOpenBet()}>
-            Open Bets
-          </div>
-          <div className=`${betTab === 'close' ? 'bg-green-200' : 'bg-white'} rounded-lg border-4 border-green-500 text-center hover:border-green-300 p-4 w-1/2` onClick={() => handleCloseBet()} >
-            Closed Bets
-          </div>
-        </div>
+  <div className="md:w-4/5 w-11/12">
+    <div className="bg-gray-400 rounded-lg p-4 flex gap-4">
+      <div
+        className={`${betTab === 'open' ? 'bg-green-200' : 'bg-white'} text-gray-700 text-center rounded-lg border-4 border-green-500 hover:border-green-200 p-4 w-1/2`}
+        onClick={() => handleOpenBet()}
+      >
+        Open Bets
       </div>
-      <div>
-        {bet.length > 0 && bet.map((item) => (
-          item.status
-        ))}
+      <div
+        className={`${betTab === 'close' ? 'bg-green-200' : 'bg-white'} rounded-lg border-4 border-green-500 text-center hover:border-green-300 p-4 w-1/2`}
+        onClick={() => handleCloseBet()}
+      >
+        Closed Bets
       </div>
     </div>
+  </div>
+  <div>
+    {bet.length > 0 &&
+      bet.map((item, index) => (
+        <div key={index}>{item.status}</div>
+      ))}
+  </div>
+</div>
     );
 }
