fe679ffe0b90740d8ea646e21f6fce3b690e7919
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-01 13:10:43 +0100

    Update games.tsx

---
+++ bet/app/components/games.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useDispatch, useSelector } from 'react-redux';
+import { mainStateReducer } from '@/store/slices/mainslice';
+
+export default function Games() {
+  return (
+    <div></div>
+    );
+}
