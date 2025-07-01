854a3a960ea5fe6d4924f4ce7e243d156ac753f8
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-01 11:17:38 +0100

    Update route.js

--- bet/app/api/postbet/route.js
+++ bet/app/api/postbet/route.js
@@ -1,20 +1,38 @@
 import dbClient from '../../../db';
 import { NextResponse } from 'next/server';
 import redisClient from '../../../redis';
+import { v4 } from 'uuid';
 
-export async function GET(request) {
+const makeID = () => {
+	return v4();
+};
+
+
+export async function POST(request) {
 	const dd = await request;
-	try {
-        const tok = dd.headers.get("tok");
-	    if (!tok) { return  NextResponse.json('error', {status: 400});}
-    	const usr_id = await redisClient.get(`auth_${tok}`);
-	    if (!usr_id) {
-		    return  NextResponse.json('error', {status: 401});
-	    }
-	    const user = await dbClient.client.db().collection('users')
-    	.findOne({ "userID": usr_id });
-	    if (!user) { return  NextResponse.json('error', {status: 401});}
-        return  NextResponse.json({email: user.email}, {status: 201});
+try {
+        const tobet = JSON.parse(dd.headers.get('tobet'));
+	const tok = dd.headers.get('tok');
+        if (!tok || !tobet) { return NextResponse.json('error', {status: 400});}
+        const usr_id = await redisClient.get(`auth_${tok}`);
+        if (!usr_id) {
+            return  NextResponse.json('error', {status: 401});
+        }
+	const now = new Date();
+	const year = now.getFullYear().toString();
+	const month = (now.getMonth() + 1).toString().padStart(2, '0');
+	const day = now.getDate().toString().padStart(2, '0');
+	const hour = now.getHours().toString().padStart(2, '0');
+	const minute = now.getMinutes().toString().padStart(2, '0');
+	const second = now.getSeconds().toString().padStart(2, '0');
+	const dt = `${year}${month}${day}${hour}${minute}${second}`;
+	const gameID = makeID();
+	const result = await (await dbClient.client.db().collection('bets'))
+	.insertOne({userID: usr_id, gameID: gameID, time: dt, bet: tobet,});
+	if (result) {
+		return NextResponse.json({message: "Game Booked Successfully"}, {status: 201});
+	}
+	return NextResponse.json('error', {status: 401});
     } catch {
         return  NextResponse.json('error', {status: 400});
     }
