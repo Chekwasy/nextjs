24977639b002bf9a63f1e840e7ee8aaeff178558
Richard Chekwas <richardchekwas@gmail.com>
2025-07-07 16:58:58 +0100

    update

--- bet/app/components/bets.tsx
+++ bet/app/components/bets.tsx
@@ -91,32 +91,32 @@ export default function Bets() {
         </div>
       </div>
       <div className="md:w-4/5 w-11/12">
-        {(bet[0].status !== '') && bet.map((item: Bet, index: number) => (<div key={index} className="bg-gray-400 rounded-lg w-full md:w-4/5 lg:w-7/10 xl:w-7/10 mx-auto p-4">
+        {(bet[0].status !== '') && bet.map((item: Bet, index: number) => (<div key={index} className="bg-gray-200 rounded-lg w-full md:w-4/5 lg:w-7/10 xl:w-7/10 mx-auto p-4">
           <div className="flex flex-col space-y-4">
-            <div className="flex-row space-x-4">
+            <div className="">
               <div className={`w-3/10 p-4 font-bold rounded-lg ${item.status === 'close' ? (item.result === 'Won' ? 'bg-green-500 text-white' : 'bg-red-500 text-white') : 'bg-yellow-200'}`}>{item.result}</div>
               <div className="w-6/10 bg-blue-200 p-4 rounded-lg font-bold flex justify-end">{`${item.date.substring(6, 8)} - ${item.date.substring(4, 6)} - ${item.date.substring(0, 4)}    ${item.time.substring(0, 2)} : ${item.time.substring(2, 4)}`}</div>
             </div>
             <div className="flex flex-col space-y-4">
-              <div className="bg-green-200 flex-row space-x-4 p-4 rounded-lg">
+              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
                 {item.bet && `${item.bet[0].hometeam} vs ${item.bet[0].awayteam} ...`}
               </div>
-              <div className="bg-green-200 flex-row space-x-4 p-4 rounded-lg">
+              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
                 <div className="p-4 w-3/10">Amount Booked</div>
                 <div className="p-4 w-6/10 font-bold">{item.betamt}</div>
               </div>
 
-              <div className="bg-green-200 flex-row space-x-4 p-4 rounded-lg">
+              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
                 <div className="p-4 w-3/10">Odds</div>
                 <div className="p-4 w-6/10 font-bold">{item.odds}</div>
               </div>
 
-              <div className="bg-green-200 flex-row space-x-4 p-4 rounded-lg">
+              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
                 <div className="p-4 w-3/10">Expected Winnings</div>
                 <div className="p-4 w-6/10 font-bold">{item.potwin}</div>
               </div>
 
-              <div className="bg-green-200 flex-row space-x-4 p-4 rounded-lg">
+              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
                 <div className="p-4 w-3/10">Returns</div>
                 <div className="p-4 w-6/10 font-bold">{item.returns}</div>
               </div>
