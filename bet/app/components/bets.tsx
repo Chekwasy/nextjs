674107096dcaadd8e9397b5f3cf884e8b259daeb
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-07 18:23:46 +0100

    Update bets.tsx

--- bet/app/components/bets.tsx
+++ bet/app/components/bets.tsx
@@ -79,7 +79,7 @@ export default function Bets() {
     handleOpenBet();
   }, []);
   return (
-    <div className="flex-col justify-center items-center mt-16">
+    <div className="flex-col w-full justify-center items-center mt-16">
       <div className="md:w-4/5 w-11/12 items-center justify-center">
         <div className="bg-gray-400 rounded-lg p-4 flex gap-4">
           <div className={`${betTab === 'open' ? 'bg-green-200' : 'bg-white'} text-gray-700 text-center rounded-lg border-4 border-green-500 hover:border-green-200 p-4 w-1/2`} onClick={() => handleOpenBet()}>
@@ -93,32 +93,32 @@ export default function Bets() {
       <div className="md:w-4/5 w-11/12">
         {(bet[0].status !== '') && bet.map((item: Bet, index: number) => (<div key={index} className="bg-gray-200 rounded-lg w-full md:w-4/5 lg:w-7/10 xl:w-7/10 mx-auto p-4">
           <div className="flex flex-col space-y-4">
-            <div className="">
-              <div className={`w-3/10 p-4 font-bold rounded-lg ${item.status === 'close' ? (item.result === 'Won' ? 'bg-green-500 text-white' : 'bg-red-500 text-white') : 'bg-yellow-200'}`}>{item.result}</div>
-              <div className="w-6/10 bg-blue-200 p-4 rounded-lg font-bold flex justify-end">{`${item.date.substring(6, 8)} - ${item.date.substring(4, 6)} - ${item.date.substring(0, 4)}    ${item.time.substring(0, 2)} : ${item.time.substring(2, 4)}`}</div>
+            <div className="rounded-lg p-1 flex gap-4">
+              <div className={`w-1/2 p-4 font-bold rounded-lg ${item.status === 'close' ? (item.result === 'Won' ? 'bg-green-500 text-white' : 'bg-red-500 text-white') : 'bg-yellow-200'}`}>{item.result}</div>
+              <div className="w-1/2 bg-blue-200 p-4 rounded-lg font-bold flex justify-end">{`Date: ${item.date.substring(6, 8)} - ${item.date.substring(4, 6)} - ${item.date.substring(0, 4)}    Time: ${item.time.substring(0, 2)} : ${item.time.substring(2, 4)}`}</div>
             </div>
-            <div className="flex flex-col space-y-4">
-              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
+            <div className="flex flex-col space-y-1">
+              <div className="bg-green-200 rounded-lg p-1 text-center font-bold flex gap-4">
                 {item.bet && `${item.bet[0].hometeam} vs ${item.bet[0].awayteam} ...`}
               </div>
-              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
-                <div className="p-4 w-3/10">Amount Booked</div>
-                <div className="p-4 w-6/10 font-bold">{item.betamt}</div>
+              <div className="bg-green-200 rounded-lg p-1 flex gap-4">
+                <div className=" w-1/2">Amount Booked</div>
+                <div className=" w-1/2 font-bold text-end">{item.betamt.toLocaleString()}</div>
               </div>
 
-              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
-                <div className="p-4 w-3/10">Odds</div>
-                <div className="p-4 w-6/10 font-bold">{item.odds}</div>
+              <div className="bg-green-200 rounded-lg p-1 flex gap-4">
+                <div className=" w-1/2">Odds</div>
+                <div className=" w-1/2 font-bold text-end">{item.odds}</div>
               </div>
 
-              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
-                <div className="p-4 w-3/10">Expected Winnings</div>
-                <div className="p-4 w-6/10 font-bold">{item.potwin}</div>
+              <div className="bg-green-200 rounded-lg p-1 flex gap-4">
+                <div className=" w-1/2">Expected Winnings</div>
+                <div className=" w-1/2 font-bold text-end">{item.potwin.toLocaleString()}</div>
               </div>
 
-              <div className="bg-green-200 rounded-lg p-4 flex gap-4">
-                <div className="p-4 w-3/10">Returns</div>
-                <div className="p-4 w-6/10 font-bold">{item.returns}</div>
+              <div className="bg-green-200 rounded-lg p-1 flex gap-4">
+                <div className=" w-1/2">Returns</div>
+                <div className=" w-1/2 font-bold text-end">{item.returns.toLocaleString()}</div>
               </div>
             </div>
           </div>
