3d7640ba21a95484d6bfbb2b5c73986d3bb9c340
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-13 06:40:09 +0100

    Update bets.tsx

--- bet/app/components/bets.tsx
+++ bet/app/components/bets.tsx
@@ -162,7 +162,7 @@ export default function Bets() {
         </div>))}
       </div>)}
       {isOpen && (
-            <div className=" bg-gray-200 flex md:w-4/5 w-11/12 mx-auto">
+            <div className=" bg-gray-200 flex flex-col md:w-4/5 w-11/12 mx-auto">
                 <div className="flex justify-end">
                   <button className="text-gray-500 hover:text-gray-700" onClick={handleClose} >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
