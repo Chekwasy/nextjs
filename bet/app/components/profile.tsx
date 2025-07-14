52f963f39af4d77d00b0c5601e7d3f2cb2d83e0a
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-14 20:21:25 +0100

    Update profile.tsx

--- bet/app/components/profile.tsx
+++ bet/app/components/profile.tsx
@@ -1,20 +1,32 @@
 "use client"
-import { useState, useEffect, } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
 import { StoreState } from '../tools/s_interface';
-import { useDispatch, useSelector } from 'react-redux';
-import { mainStateReducer } from '@/store/slices/mainslice';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
-  //usedispatch to be able to write to store
-    const dispatch = useDispatch();
   //useSelector to extract what is in the store
   const storeItems: StoreState = useSelector((state) => state) as StoreState;
   return (
     <div className="flex-col w-full justify-center items-center mt-16">
       <div className=" bg-gray-200 flex flex-col md:w-4/5 w-11/12 mx-auto">
-        
+        <h2 className="text-2xl font-bold mb-4">User Profile</h2>
+    <div className="flex flex-col md:flex-row justify-between mb-4">
+      <div className="flex flex-col w-full md:w-1/2 mb-4 md:mb-0">
+        <p className="text-lg font-bold mb-2">Account Information</p>
+        <p className="text-md mb-2">Name: {storeItems?.mainSlice.me.fname} {storeItems?.mainSlice.me.lname}</p>
+        <p className="text-md mb-2">Email: {storeItems?.mainSlice.me.email}</p>
+        <p className="text-md mb-2">Mobile: {storeItems?.mainSlice.me.mobile}</p>
+        <p className="text-md mb-2">Account Balance: {storeItems?.mainSlice.me.accbal} {storeItems?.mainSlice.me.currency}</p>
+      </div>
+      <div className="flex flex-col w-full md:w-1/2">
+        <p className="text-lg font-bold mb-2">Gaming Statistics</p>
+        <p className="text-md mb-2">Nickname: {storeItems?.mainSlice.me.nickname}</p>
+        <p className="text-md mb-2">Rating: {storeItems?.mainSlice.me.rating}%</p>
+        <p className="text-md mb-2">Subscription: {storeItems?.mainSlice.me.sub}</p>
+        <p className="text-md mb-2">Total Games: {storeItems?.mainSlice.me.TGames}</p>
+        <p className="text-md mb-2">Total Won: {storeItems?.mainSlice.me.TWon}</p>
+        <p className="text-md mb-2">Total Lost: {storeItems?.mainSlice.me.TLost}</p>
+      </div>
+    </div>
       </div>
     </div>
     );
