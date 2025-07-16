967fc47011cd3f4abd34068b32d59c4035603fcf
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-16 14:14:27 +0100

    Update two2win.tsx

--- bet/app/components/two2win.tsx
+++ bet/app/components/two2win.tsx
@@ -16,6 +16,22 @@ export default function Two2Win() {
       <div className=" bg-gray-200 flex flex-col md:w-4/5 w-11/12 mx-auto">
       
       </div>
+      {isCalenderOpen && (
+            <div className=" fixed top-0 left-0 w-full h-full bg-transparent flex items-center justify-center">
+              <div className=" bg-white rounded-lg shadow-md p-8 w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4" >
+                <div className="flex justify-end">
+                  <button className="text-gray-500 hover:text-gray-700" onClick={handleCalenderClose} >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+                <div className="flex flex-col b-2 border-gray-700 bg-white p-1">
+                  <div className=""></div>
+                </div>
+              </div>
+            </div>
+          )}
     </div>
     );
 }
