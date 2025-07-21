20544d2040a29c4cecdbfb26930f8c72a23ef285
Richard Chekwas <richardchekwas@gmail.com>
2025-07-21 22:24:14 +0100

    update

--- bet/app/components/about.tsx
+++ bet/app/components/about.tsx
@@ -27,14 +27,14 @@ export default function About() {
   }, []);
   return (
     <div className="flex-col w-full justify-center items-center mt-16">
-      <div className=" bg-gray-200 flex flex-col p-2 md:w-4/5 w-11/12 mx-auto">
-        <div className='flex w-1/2 text-center font-bold p-4 font-bold rounded-lg shadow-md bg-blue-500 text-white b-2 border-gray-400 text-2xl mx-auto'>About Us</div>
+      <div className=" bg-gray-200 mb-3 flex flex-col p-2 md:w-4/5 w-11/12 mx-auto">
+        <div className='flex text-center font-bold p-4 font-bold rounded-lg shadow-md bg-blue-500 text-white b-2 border-gray-400 text-xl'>About Us</div>
       </div>
-      <div className="bg-gray-200 flex flex-col md:w-4/5 w-11/12 mx-auto">
+      <div className="bg-gray-200 flex mb-3 flex-col md:w-4/5 w-11/12 mx-auto">
         {about.map((item, index) => (
           <div key={index}>
-            <div className='flex font-bold p-4 rounded-lg shadow-md text-white text-lg bg-gray-500'>{item.title}</div>
-            <p className='flex p-4 bg-green-200 rounded-lg text-gray-800'>{item.body}</p>
+            <div className='flex p-4 rounded-lg mb-3 shadow-md text-white text-lg bg-gray-500'>{item.title}</div>
+            <p className='flex p-4 bg-green-200 rounded-lg text-gray-800 mb-3'>{item.body}</p>
           </div>
         ))}
       </div>
