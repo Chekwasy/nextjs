2a5e6ce72d4fbc5aff56f6498d166463f7c8e6a6
Chekwasy <49874027+Chekwasy@users.noreply.github.com>
2025-07-22 18:18:40 +0100

    Update route.js

--- bet/app/api/gettwo2win/route.js
+++ bet/app/api/gettwo2win/route.js
@@ -5,9 +5,11 @@ export async function GET(request) {
     const dd = await request;
     try {
         const date = dd.headers.get("date");
+        console.log(date);
         if (!date) { return  NextResponse.json('error', {status: 400});}
         const game = await dbClient.client.db().collection('two2win')
         .findOne({ "date": date });
+        console.log(game);
         if (!game) { return  NextResponse.json({game: null, message: "Success" }, {status: 201});}
         return  NextResponse.json({game: game.game, message: "Success" }, {status: 201});
     } catch {
