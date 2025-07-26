0e18a79ff60bd0603bddbc8af3b80d00387c48ec
Richard Chekwas <richardchekwas@gmail.com>
2025-07-27 00:25:25 +0100

    update

--- bet/app/api/gettwo2win/route.js
+++ bet/app/api/gettwo2win/route.js
@@ -8,6 +8,7 @@ export async function GET(request) {
         const url = new URL(dd.url);
         const date = url.searchParams.get('date');
         const tok = dd.headers.get('tok');
+        console.log(tok);
         if (!tok) {return  NextResponse.json('error', {status: 400});}
         const usr_id = await redisClient.get(`auth_${tok}`);
         if (!usr_id) {
@@ -16,7 +17,9 @@ export async function GET(request) {
 		const usr = await dbClient.client.db().collection('users')
 		.findOne({ "userID": usr_id });
 		if (!usr) {return  NextResponse.json('error', {status: 401});}
+        console.log(usr.sub.slice(-8));
         if (isDateInPast(usr.sub.slice(-8))) {
+            console.log('nil');
             return  NextResponse.json({game: null, message: "Success" }, {status: 201});
         }
         if (!date) { return  NextResponse.json('error', {status: 400});}
