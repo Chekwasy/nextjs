8b048996abc9d44134c4caf6cadbcab3a0142fbd
Richard Chekwas <richardchekwas@gmail.com>
2025-08-16 09:17:59 +0100

    update

--- bet/app/api/getgames/route.ts
+++ bet/app/api/getgames/route.ts
@@ -30,101 +30,77 @@ export async function GET(request: Request) {
 	if (dateLst[0].length === 1) {dateLst[0] = '0' + dateLst[0];}
 	if (dateLst[1].length === 1) {dateLst[1] = '0' + dateLst[1];}
 	const date_ = dateLst[2] + dateLst[0] + dateLst[1];
-	console.log(date_);
-	//scrap the matches data
-	const response = await axios.get(`https://prod-public-api.livescore.com/v1/api/app/date/soccer/${date_}/1?countryCode=NG&locale=en&MD=1`);
-	const gamesJson = response.data;
-	// extract details for all events that is active 
-	const gjLen = gamesJson.Stages.length;
+	const sGames = await dbClient.client.db().collection('dates')
+    .findOne({ "date":date_ });
 	const oddLst = [];
-	let eventDit: { 
- 	 id: string, 
- 	 titleCountry: string, 
- 	 subtitle: string, 
- 	 events: { 
-  	  id: string, 
-  	  hometeam: string, 
-   	 awayteam: string, 
-   	 homeodd: string, 
-  	  awayodd: string, 
- 	   drawodd: string, 
-   	 Esd: string 
-  	}[] 
-	} = {
-  	id: '',
-  	titleCountry: '',
-	  subtitle: '',
- 	 events: []
-	};
-	for (let i = 0; i < gjLen; i++) {
-		const evtLen = gamesJson.Stages[i].Events.length;
-		eventDit["id"] = i.toString();
-		eventDit["titleCountry"] = gamesJson.Stages[i].Cnm;
-		eventDit["subtitle"] = gamesJson.Stages[i].Snm;
-		eventDit["events"] = [];
-		for (let j = 0; j < evtLen; j++) {
-			if (gamesJson.Stages[i].Events[j].Eps === 'NS') {
-				const Edt: { 
-    				id: string, 
-    				hometeam: string, 
-   				 awayteam: string, 
-    				homeodd: string, 
-    				awayodd: string, 
-   				 drawodd: string, 
-   				 Esd: string 
-				} = {
-  				id: '',
- 				 hometeam: '',
- 				 awayteam: '',
-  				homeodd: '',
-  				awayodd: '',
- 				 drawodd: '',
- 				 Esd: ''
-				};
-				if (givenDate === 0) {
-					const hr1 = today.getHours();
-					const mn1 = today.getMinutes();
-					const hr2 = parseInt(gamesJson.Stages[i].Events[j].Esd.toString().substring(8, 10));
-					const mn2 = parseInt(gamesJson.Stages[i].Events[j].Esd.toString().substring(10, 12));
-					if (hr2 > hr1) {
-						Edt["id"] = j.toString();
-						Edt['hometeam'] = gamesJson.Stages[i].Events[j].T1[0].Nm;
-						Edt['awayteam'] = gamesJson.Stages[i].Events[j].T2[0].Nm;
-						
-						const Team1 = `${gamesJson.Stages[i].Events[j].T1[0].Nm}`;
-						const Team2 = `${gamesJson.Stages[i].Events[j].T2[0].Nm}`;
-						const Team1L = findLongestWord(Team1);
-						const Team2L = findLongestWord(Team2);
-						const bothTeam = `${Team1L}=${Team2L}`;
-						const oddG = await searchAndPrintLastChars(bothTeam, 'output.txt');
-						if (oddG !== '') {
-
-							const splitStr = oddG.split(' ').reverse();
-							Edt['homeodd'] = splitStr[2];
-							Edt['drawodd'] = splitStr[1];
-							Edt['awayodd'] = splitStr[0];	
-						} else {
-							Edt['homeodd'] = '1.7';
-							Edt['drawodd'] = '3.1';
-							Edt['awayodd'] = '1.8';
-						}
-
-						Edt['Esd'] = gamesJson.Stages[i].Events[j].Esd.toString();
-						eventDit['events'].push(Edt);
-					}
-					if (hr2 === hr1) {
-						if (mn2 > mn1) {
+	if (!sGames) {
+		//scrap the matches data
+		const response = await axios.get(`https://prod-public-api.livescore.com/v1/api/app/date/soccer/${date_}/1?countryCode=NG&locale=en&MD=1`);
+		const gamesJson = response.data;
+		// extract details for all events that is active 
+		const gjLen = gamesJson.Stages.length;
+		let eventDit: { 
+		id: string, 
+		titleCountry: string, 
+		subtitle: string, 
+		events: { 
+		id: string, 
+		hometeam: string, 
+		awayteam: string, 
+		homeodd: string, 
+		awayodd: string, 
+		drawodd: string, 
+		Esd: string 
+		}[] 
+		} = {
+		id: '',
+		titleCountry: '',
+		subtitle: '',
+		events: []
+		};
+		for (let i = 0; i < gjLen; i++) {
+			const evtLen = gamesJson.Stages[i].Events.length;
+			eventDit["id"] = i.toString();
+			eventDit["titleCountry"] = gamesJson.Stages[i].Cnm;
+			eventDit["subtitle"] = gamesJson.Stages[i].Snm;
+			eventDit["events"] = [];
+			for (let j = 0; j < evtLen; j++) {
+				if (gamesJson.Stages[i].Events[j].Eps === 'NS') {
+					const Edt: { 
+						id: string, 
+						hometeam: string, 
+					awayteam: string, 
+						homeodd: string, 
+						awayodd: string, 
+					drawodd: string, 
+					Esd: string 
+					} = {
+					id: '',
+					hometeam: '',
+					awayteam: '',
+					homeodd: '',
+					awayodd: '',
+					drawodd: '',
+					Esd: ''
+					};
+					if (givenDate === 0) {
+						const hr1 = today.getHours();
+						const mn1 = today.getMinutes();
+						const hr2 = parseInt(gamesJson.Stages[i].Events[j].Esd.toString().substring(8, 10));
+						const mn2 = parseInt(gamesJson.Stages[i].Events[j].Esd.toString().substring(10, 12));
+						if (hr2 > hr1) {
 							Edt["id"] = j.toString();
 							Edt['hometeam'] = gamesJson.Stages[i].Events[j].T1[0].Nm;
 							Edt['awayteam'] = gamesJson.Stages[i].Events[j].T2[0].Nm;
-
+							
 							const Team1 = `${gamesJson.Stages[i].Events[j].T1[0].Nm}`;
 							const Team2 = `${gamesJson.Stages[i].Events[j].T2[0].Nm}`;
 							const Team1L = findLongestWord(Team1);
 							const Team2L = findLongestWord(Team2);
 							const bothTeam = `${Team1L}=${Team2L}`;
-							const oddG: string = await searchAndPrintLastChars(bothTeam, 'output.txt');
+							const oddG = await searchAndPrintLastChars(bothTeam, 'output.txt');
 							if (oddG !== '') {
+
 								const splitStr = oddG.split(' ').reverse();
 								Edt['homeodd'] = splitStr[2];
 								Edt['drawodd'] = splitStr[1];
@@ -138,55 +114,82 @@ export async function GET(request: Request) {
 							Edt['Esd'] = gamesJson.Stages[i].Events[j].Esd.toString();
 							eventDit['events'].push(Edt);
 						}
-					}
-				} else {
-					Edt["id"] = j.toString();
-					Edt['hometeam'] = gamesJson.Stages[i].Events[j].T1[0].Nm;
-					Edt['awayteam'] = gamesJson.Stages[i].Events[j].T2[0].Nm;
-					
-					const Team1 = `${gamesJson.Stages[i].Events[j].T1[0].Nm}`;
-					const Team2 = `${gamesJson.Stages[i].Events[j].T2[0].Nm}`;
-					const Team1L = findLongestWord(Team1);
-					const Team2L = findLongestWord(Team2);
-					const bothTeam = `${Team1L}=${Team2L}`;
-					const oddG: string = await searchAndPrintLastChars(bothTeam, 'output.txt');
-					if (oddG !== '') {
-						const splitStr = oddG.split(' ').reverse();
-						Edt['homeodd'] = splitStr[2];
-						Edt['drawodd'] = splitStr[1];
-						Edt['awayodd'] = splitStr[0];	
+						if (hr2 === hr1) {
+							if (mn2 > mn1) {
+								Edt["id"] = j.toString();
+								Edt['hometeam'] = gamesJson.Stages[i].Events[j].T1[0].Nm;
+								Edt['awayteam'] = gamesJson.Stages[i].Events[j].T2[0].Nm;
+
+								const Team1 = `${gamesJson.Stages[i].Events[j].T1[0].Nm}`;
+								const Team2 = `${gamesJson.Stages[i].Events[j].T2[0].Nm}`;
+								const Team1L = findLongestWord(Team1);
+								const Team2L = findLongestWord(Team2);
+								const bothTeam = `${Team1L}=${Team2L}`;
+								const oddG: string = await searchAndPrintLastChars(bothTeam, 'output.txt');
+								if (oddG !== '') {
+									const splitStr = oddG.split(' ').reverse();
+									Edt['homeodd'] = splitStr[2];
+									Edt['drawodd'] = splitStr[1];
+									Edt['awayodd'] = splitStr[0];	
+								} else {
+									Edt['homeodd'] = '1.7';
+									Edt['drawodd'] = '3.1';
+									Edt['awayodd'] = '1.8';
+								}
+
+								Edt['Esd'] = gamesJson.Stages[i].Events[j].Esd.toString();
+								eventDit['events'].push(Edt);
+							}
+						}
 					} else {
-						Edt['homeodd'] = '1.7';
-						Edt['drawodd'] = '3.1';
-						Edt['awayodd'] = '1.8';
+						Edt["id"] = j.toString();
+						Edt['hometeam'] = gamesJson.Stages[i].Events[j].T1[0].Nm;
+						Edt['awayteam'] = gamesJson.Stages[i].Events[j].T2[0].Nm;
+						
+						const Team1 = `${gamesJson.Stages[i].Events[j].T1[0].Nm}`;
+						const Team2 = `${gamesJson.Stages[i].Events[j].T2[0].Nm}`;
+						const Team1L = findLongestWord(Team1);
+						const Team2L = findLongestWord(Team2);
+						const bothTeam = `${Team1L}=${Team2L}`;
+						const oddG: string = await searchAndPrintLastChars(bothTeam, 'output.txt');
+						if (oddG !== '') {
+							const splitStr = oddG.split(' ').reverse();
+							Edt['homeodd'] = splitStr[2];
+							Edt['drawodd'] = splitStr[1];
+							Edt['awayodd'] = splitStr[0];	
+						} else {
+							Edt['homeodd'] = '1.7';
+							Edt['drawodd'] = '3.1';
+							Edt['awayodd'] = '1.8';
+						}
+						Edt['Esd'] = gamesJson.Stages[i].Events[j].Esd.toString();
+						eventDit['events'].push(Edt);
 					}
-					Edt['Esd'] = gamesJson.Stages[i].Events[j].Esd.toString();
-					eventDit['events'].push(Edt);
 				}
 			}
+			if (eventDit.events.length > 0) {
+				oddLst.push(eventDit);
+			}
+			eventDit = {} as { 
+			id: string, 
+			titleCountry: string, 
+			subtitle: string, 
+			events: { 
+			id: string, 
+			hometeam: string, 
+			awayteam: string, 
+			homeodd: string, 
+			awayodd: string, 
+			drawodd: string, 
+			Esd: string 
+			}[] 
+			};
 		}
-		if (eventDit.events.length > 0) {
-			oddLst.push(eventDit);
-		}
-		eventDit = {} as { 
-  		id: string, 
-  		titleCountry: string, 
-  		subtitle: string, 
- 		 events: { 
-  		  id: string, 
-  		  hometeam: string, 
- 		   awayteam: string, 
- 		   homeodd: string, 
- 		   awayodd: string, 
- 		   drawodd: string, 
- 		   Esd: string 
-		  }[] 
-		};
+		//Save data to db
+		await dbClient.client.db().collection('dates')
+		.insertOne({"date": date_, "games": oddLst
+		});
 	}
-	//Save data to db
-	await (await dbClient.client.db().collection('dates'))
-	.insertOne({"date": date_, "games": oddLst
-	});
   	const dates : {date: string; indent: number;}[] = [];
  	 const todayy = new Date();
 
@@ -197,7 +200,7 @@ export async function GET(request: Request) {
 
  	   dates.push({ date: dateString, indent: i });
 	  }
-	return NextResponse.json({date: date_, datee: dates, games: oddLst}, {status: 201});
+	return NextResponse.json({date: date_, datee: dates, games: (oddLst.length === 0 && sGames) ? sGames.games : oddLst}, {status: 201});
     } catch {
 	    return NextResponse.json('error fetching data', {status: 400});
     }
