df4fd2fc3fe3f6f2a73f890be1744972f7b6d2f4
Richard Chekwas <richardchekwas@gmail.com>
2025-08-27 09:56:49 +0100

    update

--- bet/app/components/two2win.tsx
+++ bet/app/components/two2win.tsx
@@ -152,7 +152,7 @@ export default function Two2Win() {
                             alt="Two2Win"
                             width={24}
                             height={24}
-                            className="filter brightness-0 invert mr-3"
+                            className="mr-3"
                         />
                         <h2 className="ml-2 text-xl font-semibold text-green-950">
                             Two2Win
